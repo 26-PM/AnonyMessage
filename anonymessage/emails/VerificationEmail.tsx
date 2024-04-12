e066cd238ae2ab47ced7aa0cb589bb38af88dbc7
PM <121531128+26-PM@users.noreply.github.com>
2024-04-12 11:07:48 +0000

    added route of signup

---
+++ anonymessage/emails/VerificationEmail.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+interface EmailTemplateProps {
+  username: string;
+  otp: string;
+}
+
+export default function EmailTemplate({username,otp}:EmailTemplateProps){
+  return(
+  <div>
+    <h1>Welcome, {username}!</h1>
+    <p>
+      Thank you for signing up for our service. We hope you will enjoy using it!
+    </p>
+    <p>Please use the following verification code to complete your registration :</p>
+    <p>{otp}</p>
+
+  </div>
+);
+}
